56bee583c442cb1a9e784f4f5007c80681c498e0
Raneem <aljamal.raneem@gmail.com>
2023-02-17 22:26:58 +0200

    step2 2

---
+++ 2/step2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     error_reporting(E_ALL);
+     ini_set('display_errors', 0);
+     ini_set('log_errors', 1);
+     ini_set('error_log', '/var/log/php-errors.log');
+      
+   function greaterThen($num) {
+     if($num > 30){
+        echo"$num greater than 30<br>";
+     }if($num > 20 && $num < 30){
+        echo"$num greater than 20<br>";
+     }if($num > 10 && $num < 20){
+        echo"$num greater than 10<br>";
+     }if($num < 10){
+        echo"$num is less than 10<br>";
+     }
+
+   }
+   greaterThen(40);
+   greaterThen(21);
+   greaterThen(12);
+   greaterThen(8);
+
+     $result = 10 / 0;
+    
+    ?>
+</body>
+</html>
