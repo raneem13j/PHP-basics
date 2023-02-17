3ad74299daf189bd26dcf983ecbcbba96827bc21
Raneem <aljamal.raneem@gmail.com>
2023-02-17 16:19:58 +0200

    step4

---
+++ step 4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+      $name = "Raneem Aljamal";
+      $position= "full stack developer";
+      $github_url="https://github.com/raneem13j/PHP-basics";
+      echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link ".$github_url.".<br>";
+      echo "Hello, I'm $name, I'm a $position please check my github link $github_url.<br>";
+      echo 'Hello, I\'m '.$name.', I\'m a '.$position.' please check my github link '.$github_url.'.<br>';
+      ?>
+</body>
+</html>
